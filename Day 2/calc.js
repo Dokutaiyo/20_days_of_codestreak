dd4d9f306b096db0b8cbf80b4e48f89dbee99eb0
Dokutaiyo <dokutaiyo@gmail.com>
2023-01-10 00:43:13 +0530

    Day 2

---
+++ Day 2/calc.js
@@ -0,0 +1,25 @@
+// Define a function to add two numbers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Define a function to subtract two numbers
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Define a function to multiply two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Define a function to divide two numbers
+  function divide(a, b) {
+    return a / b;
+  }
+  
+// Test the functions by calling them and passing in different values for the arguments
+console.log(add(1, 2)); // Output: 3
+console.log(subtract(5, 10)); // Output: -5
+console.log(multiply(-3, 7)); // Output: -21
+console.log(divide(4, 2)); // Output: 2 
